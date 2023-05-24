bda8f80144a6155065009f17ff58a9ece1e48064
bananak1n99 <lcvi.ktpm0217@student.ctuet.edu.vn>
2023-05-24 11:30:39 +0700

    133

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../redux/features/auth/authSlice";
-
-
+import productReducer from "../redux/features/auth/product/productSlice";
+// import filterReducer from "../redux/features/product/filterSlice";
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-
-    },
-
+  reducer: {
+    auth: authReducer,
+    product: productReducer,
+    // filter: filterReducer,
+  },
 });
