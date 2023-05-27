6ab902e2950bf91532f19ec18421f40eeaa956fa
bananak1n99 <lcvi.ktpm0217@student.ctuet.edu.vn>
2023-05-27 11:09:00 +0700

    Update productController.js

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+const dontenv  = require("dotenv");
 const asyncHandler = require("express-async-handler");
 const Product = require("../models/productModel");
 const { fileSizeFormatter } = require("../utils/fileUpload");
